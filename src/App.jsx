4b4a213c14c5b4354108e794696419c9404eacae
mabdullahw70-tech <mabdullahw.70@gmail.com>
2026-05-04 20:35:31 +0500

    cart added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Newsingle from "./pages/Newsingle";
 import News from "./pages/News";
+import Cart from "./pages/Cart";
 
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/news" element={<News />} />
         {/*<Route path="/contact" element={<Contact />} />
         <Route path="/shop" element={<Shop />} /> */}
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
