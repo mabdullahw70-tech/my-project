67b95209f9c8eee830f0688640f5f42402e81963
shoaibmhr <shoaibmhr895@gmail.com>
2026-05-07 11:50:44 +0500

    changing

--- src/components/CommonHero.jsx
+++ src/components/CommonHero.jsx
@@ -1,16 +1,20 @@
 export default function CommonHero({ intro, mainheading, bgImage }) {
   return (
     <section
-      className="bg-[#364955] h-[80vh] flex items-center justify-center pt-16 bg-cover bg-center bg-no-repeat"
+      className="relative bg-[#364955] h-[80vh] flex items-center justify-center pt-16 bg-cover bg-top bg-no-repeat"
       style={{
-        backgroundImage: `url(${bgImage})
-        `,
+        backgroundImage: `url(${bgImage})`,
       }}
     >
-      <div className="text-center">
-        <p className="text-2xl text-orange-500  font-bold tracking-[6px] uppercase">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/40"></div>
+
+      {/* Content */}
+      <div className="relative z-10 text-center">
+        <p className="text-2xl text-orange-500 font-bold tracking-[6px] uppercase">
           {intro}
         </p>
+
         <h1 className="text-white text-4xl md:text-6xl font-bold mt-4">
           {mainheading}
         </h1>
--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,11 +1,12 @@
 import CommonHero from "../components/CommonHero";
 import NotFound from "../components/NotFound";
 import Slider1 from "../components/Slider1";
+import Aboutheroimg9 from "../assets/Aboutheroimg9.jpg";
 
 export default function Page404() {
   return (
     <div>
-      <CommonHero intro="FRESH AND ORGANIC" mainheading="404 - Not Found" />
+      <CommonHero intro="FRESH AND ORGANIC" mainheading="404 - Not Found"bgImage={Aboutheroimg9}  />
       <NotFound />
       <Slider1 />
     </div>
